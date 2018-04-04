a1c58b13247d50fc55a010d056fc20bb5353129a
ziginsider <ziginsider@gmail.com>
2018-04-05 00:57:25 +0300

    onNavigationItemSelectedListener

--- app/src/main/java/io/github/ziginsider/epam_laba_9/MainActivity.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba_9/MainActivity.kt
@@ -2,11 +2,32 @@ package io.github.ziginsider.epam_laba_9
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar.*
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_fragment_first -> {
+
+            }
+            R.id.nav_fragment_second -> {
+
+            }
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+
 }
