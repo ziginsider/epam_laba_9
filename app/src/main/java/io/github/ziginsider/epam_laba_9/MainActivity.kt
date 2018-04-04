4d59516a32f89b83a162d27d72790572d184524f
ziginsider <ziginsider@gmail.com>
2018-04-05 01:15:47 +0300

    add ActionBarDrawerToggle

--- app/src/main/java/io/github/ziginsider/epam_laba_9/MainActivity.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba_9/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar.*
@@ -14,6 +15,14 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        val toggle = ActionBarDrawerToggle(
+                this,
+                drawer_layout,
+                toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        navigation_view.setNavigationItemSelectedListener(this)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
