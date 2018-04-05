7420b07865f30cde184b1a27097a65c326b3aebe
ziginsider <ziginsider@gmail.com>
2018-04-05 18:24:23 +0300

    add documentation

--- app/src/main/java/io/github/ziginsider/epam_laba_9/MainActivity.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba_9/MainActivity.kt
@@ -3,9 +3,12 @@ package io.github.ziginsider.epam_laba_9
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.view.MenuItem
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar.*
 
@@ -14,27 +17,44 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
         val toggle = ActionBarDrawerToggle(
                 this,
                 drawer_layout,
-                toolbar, R.string.navigation_drawer_open,
+                toolbar,
+                R.string.navigation_drawer_open,
                 R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
         navigation_view.setNavigationItemSelectedListener(this)
+        setNavigationDrawerHeader()
+        setFragment(FirstFragment())
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_fragment_first -> {
-
+                setFragment(FirstFragment())
             }
             R.id.nav_fragment_second -> {
-
+                setFragment(SecondFragment())
             }
         }
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit()
+    }
+
+    private fun setNavigationDrawerHeader() {
+        val v = navigation_view.inflateHeaderView(R.layout.header_layout)
+        val img = v.findViewById(R.id.image_view_header) as ImageView
+        Glide.with(img.context)
+                .load(resources.getString(R.string.url_one))
+                .into(img)
     }
 }
