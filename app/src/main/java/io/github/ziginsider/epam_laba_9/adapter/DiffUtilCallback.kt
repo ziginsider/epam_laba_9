113521d899bd6f840fdf707a3514fb51c2fd9af6
ziginsider <ziginsider@gmail.com>
2018-04-17 02:01:38 +0300

    refactoring

--- app/src/main/java/io/github/ziginsider/epam_laba_9/adapter/DiffUtilCallback.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba_9/adapter/DiffUtilCallback.kt
@@ -4,7 +4,7 @@ import android.support.v7.util.DiffUtil
 import io.github.ziginsider.epam_laba_9.model.Character
 
 class DiffUtilCallback(private val oldItems: List<Character>,
-                       private val newItems: List<Character >): DiffUtil.Callback() {
+                       private val newItems: List<Character>) : DiffUtil.Callback() {
 
     override fun getOldListSize() = oldItems.size
 
