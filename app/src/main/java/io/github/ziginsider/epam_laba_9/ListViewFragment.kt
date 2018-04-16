3475ec9c5f6efbf3d50f0f5cffb535f52a78f351
ziginsider <ziginsider@gmail.com>
2018-04-17 01:43:49 +0300

    add onScrollListener to ListView

--- app/src/main/java/io/github/ziginsider/epam_laba_9/ListViewFragment.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba_9/ListViewFragment.kt
@@ -6,10 +6,13 @@ import io.github.ziginsider.epam_laba_9.model.Character
 import io.github.ziginsider.epam_laba_9.model.MockCharacter
 import io.github.ziginsider.epam_laba_9.utils.toast
 import kotlinx.android.synthetic.main.fragment_list.*
+import android.support.design.widget.FloatingActionButton
+import android.widget.AbsListView
 
-class ListViewFragment: BaseFragment() {
+class ListViewFragment : BaseFragment() {
     override val logTag = ListViewFragment::class.java.simpleName
 
+    private val scrollListener: OnScrollListener by lazy { OnScrollListener(fab_list) }
     private var listViewAdapter: ListViewAdapter? = null
     private lateinit var characters: List<Character>
 
@@ -21,6 +24,9 @@ class ListViewFragment: BaseFragment() {
         super.onActivityCreated(savedInstanceState)
         characters = MockCharacter(resources).generateEmperorCharacters()
         setUpListView(characters)
+        fab_list.setOnClickListener {
+            activity.toast("I'm FAB from ListView !")
+        }
     }
 
     override fun onDetach() {
@@ -29,9 +35,31 @@ class ListViewFragment: BaseFragment() {
     }
 
     private fun setUpListView(items: List<Character>) {
-        listViewAdapter = ListViewAdapter(context, items, {activity.toast("I'm  $name !!")})
+        listViewAdapter = ListViewAdapter(
+                context,
+                items,
+                { activity.toast("I'm  $name !!") })
         with(listView) {
             adapter = listViewAdapter
+            setOnScrollListener(scrollListener)
         }
     }
+
+    class OnScrollListener(val fab: FloatingActionButton) : AbsListView.OnScrollListener {
+        private var lastPosition = 1
+
+        override fun onScrollStateChanged(view: AbsListView, scrollState: Int) {}
+
+        override fun onScroll(view: AbsListView?, firstVisibleItem: Int, visibleItemCount: Int,
+                              totalItemCount: Int) {
+            if (lastPosition < firstVisibleItem) {
+                fab.hide()
+            }
+            if (lastPosition > firstVisibleItem) {
+                fab.show()
+            }
+            lastPosition = firstVisibleItem
+        }
+    }
+
 }
